0b92574dee3075e14d49b4c50b1312a45c2aec0c
Vicky <waqasbilal02@gmail.com>
2022-03-12 11:55:59 +0500

    sstup.py file completed

---
+++ setup.py
@@ -0,0 +1,12 @@
+from unicodedata import name
+from setuptools import setup, find_packages
+from utility import read_params
+params=read_params('config/params.yaml')
+setup(
+    name=params['base']['project_name'],
+    version=params['base']['version'],
+    description=params["base"]['description'],
+    author=params['base']['author'],
+    packages=find_packages(),
+    license=params['base']['license'],
+)
