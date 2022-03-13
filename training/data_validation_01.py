36ce8cebdce56a696d107f48beadf9deed21541b
Vicky <waqasbilal02@gmail.com>
2022-03-13 10:59:30 +0500

    resolved [error 17]

--- training/data_validation_01.py
+++ training/data_validation_01.py
@@ -22,7 +22,7 @@ class Data_Validation:
         """Return the regular expression for the data files to make sure that it only validates the right files
         """
         return "[HealthPrem_]+[0-9]+[_]+[0-9]+.csv"
-    def move_to_bad_archive_directory(self,file_path:str):
+    def move_to_bad_archive_directory(self,folder_path,file_name:str):
         """Move the bad file to Bad Archive Directory
 
         Args:
@@ -30,23 +30,24 @@ class Data_Validation:
         """
         try:
             #problem
-            self.create_bad_data_directory(file_path)
+            self.create_bad_data_directory(folder_path)
             shutil.move(
-                file_path, self.__configself.__config['artifacts']['Data_Directories']['training']['bad_raw_data'])
+                os.path.join(folder_path, file_name), self.__configself.__config['artifacts']['Data_Directories']['training']['Bad_Data_Directory'])
         except Exception:
             error = InsuranceException("Error in module {0} class {1} method {2}".format(
                 Data_Validation.__module__.__str__, Data_Validation.__class__.__name__, self.move_to_bad_archive_directory.__name__), sys)
             raise error
-    def copy_to_good_data_directory(self,file_path:str):
+
+    def copy_to_good_data_directory(self, folder_path, file_name: str):
         """Copy the good file to Good Data Directory
 
         Args:
             file_path (str): Path of the file
         """
         try:
-            self.create_good_data_directory(file_path)
+            self.create_good_data_directory(folder_path)
             shutil.copy(
-                file_path, self.__config['artifacts']['Data_Directories']['training']['good_raw_data'])
+                os.path.join(folder_path, file_name), self.__config['artifacts']['Data_Directories']['training']['Good_Data_Directory'])
         except Exception:
             error = InsuranceException("Error in module {0} class {1} method {2}".format(
                 Data_Validation.__module__.__str__, Data_Validation.__class__.__name__, self.copy_to_good_data_directory.__name__), sys)
@@ -65,21 +66,22 @@ class Data_Validation:
             path (str): path of the Bad Data directory
         """
         utility.create_directory(path)
-    def validate_file_name(self,filepath):
-        files=os.listdir(filepath)
+    def validate_file_name(self,batch_directory_path):
+        files = os.listdir(batch_directory_path)
         LengthOfDateStampInFile, LengthOfTimeStampInFile, _, _ = utility.read_training_schema()
         match_string = self.return_regular_expression()
-        for file in files:
-            if not file.endswith('.csv'):
+        for file_name in files:
+            if not file_name.endswith('.csv'):
                 continue
-            datestamp_length=len(file.split('_')[1])
-            timestamp_length = len(file.split('_')[2].split('.csv')[0])
-            if re.match(match_string, file) and datestamp_length==LengthOfDateStampInFile and timestamp_length==LengthOfTimeStampInFile:
-                self.copy_to_good_data_directory(os.path.join(os.getcwd(),filepath, file))
+            datestamp_length = len(file_name.split('_')[1])
+            timestamp_length = len(file_name.split('_')[2].split('.csv')[0])
+            if re.match(match_string, file_name) and datestamp_length == LengthOfDateStampInFile and timestamp_length == LengthOfTimeStampInFile:
+                self.copy_to_good_data_directory(os.path.join(
+                    os.getcwd(), batch_directory_path), file_name)
                 print('copy')
             else:
                 self.move_to_bad_archive_directory(
-                    os.path.join(os.getcwd(), filepath, file))
+                    os.path.join(os.getcwd(), batch_directory_path), file_name)
                 print('move')
 
 
