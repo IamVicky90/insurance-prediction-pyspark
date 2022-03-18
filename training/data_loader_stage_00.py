96e6d1b2f5529b8bfacdb85ea1918f8f016e0928
Vicky <waqasbilal02@gmail.com>
2022-03-18 16:18:47 +0500

    removing some small errors

--- training/data_loader_stage_00.py
+++ training/data_loader_stage_00.py
@@ -3,30 +3,31 @@ from insurance_exception.insurance_exception import InsuranceException as DataLo
 from utility import read_params, get_log_object_for_training
 import argparse
 import sys
+import ast
 log_collection_name = "Data_Loader"
 class Data_Loader:
-    def __init__(self, log_collection_name:str, path:str, is_log_enabled:str):
+    def __init__(self, is_log_enabled: bool,log_collection_name: str = log_collection_name):
         """Responsible For Loading the Training Data.
 
         Args:
             log_collection_name (str): Name of the collection in MongoDb database.
-            path (str): Path of the data.
             is_log_enabled (bool): If it is set to True then only it will write the logs. Defaults to True.
         """
         self.log_collection_name = log_collection_name
-        self.logger = get_log_object_for_training(log_collection_name,is_log_enabled)
-        self.path = path
-        self.is_log_enabled = is_log_enabled
+        self.logger = get_log_object_for_training(
+            log_collection_name, is_log_enabled=is_log_enabled)
         self.ss=Spark_Manager().get_SparkSession()
-    def load_data_from_path(self):
+    def load_data_from_path(self,path):
         """Load Data From the given path using Spark DataFrame.
+        Args:
+            path (str): Path of the data.
         Returns:
             SparkSeeion.DataFrame: Spark DataFrame
         """
         try:
             self.logger.log("Loading data from path has been started")
-            df= self.ss.read.csv(self.path)
-            self.logger.log(f"Sucessfully Load the data from path {self.path}")
+            df= self.ss.read.csv(path,header=True)
+            self.logger.log(f"Sucessfully Load the data from path {path}")
             return df
         except Exception:
             error = DataLoaderException("Error in module {0} class {1} method {2}".format(
@@ -37,12 +38,16 @@ if __name__ == "__main__":
     args=argparse.ArgumentParser()
     args.add_argument("--log_collection_name", default=log_collection_name)
     args.add_argument("--is_log_enabled", default=True)
-    args.add_argument("--data_source")
+    args.add_argument(
+        "--data_source", default=["data/training_batch_files/HealthPrem_26092020_131534.csv"])
     parsed_args=args.parse_args()
+    if (parsed_args.is_log_enabled) == str:
+        parsed_args.is_log_enabled = ast.literal_eval(
+            parsed_args.is_log_enabled.title())
     logger=get_log_object_for_training(log_collection_name)
-    data_loader = Data_Loader(
-        log_collection_name, parsed_args.data_source, parsed_args.is_log_enabled)
-    df=data_loader.load_data_from_path()
+    data_loader = Data_Loader(parsed_args.is_log_enabled,
+        log_collection_name)
+    df = data_loader.load_data_from_path(parsed_args.data_source)
 
 
     
--- training/data_validation_01.py
+++ training/data_validation_01.py
@@ -3,8 +3,11 @@ import shutil
 from insurance_exception.insurance_exception import InsuranceException
 import sys
 import utility
+from utility import read_training_schema
 import os
 import argparse
+import ast
+from training.data_loader_stage_00 import Data_Loader
 log_collection_name="data_validation"
 class Data_Validation:
     def __init__(self,logger,config):
@@ -21,7 +24,7 @@ class Data_Validation:
         """Return the regular expression for the data files to make sure that it only validates the right files
         """
         return "[HealthPrem_]+[0-9]+[_]+[0-9]+.csv"
-    def move_to_bad_archive_directory(self,folder_path,file_name:str):
+    def move_to_bad_archive_directory(self,file_path:str):
         """Move the bad file to Bad Archive Directory
 
         Args:
@@ -32,15 +35,15 @@ class Data_Validation:
             destination_folder=os.path.join(Bad_Data_Directory,utility.get_date(),utility.get_time())
             self.create_bad_data_directory(destination_folder)
             shutil.move(
-                os.path.join(folder_path, file_name), destination_folder)
+                file_path, destination_folder)
             self.__logger.log(
-                f"Successfully move the file {file_name} to {destination_folder}")
+                f"Successfully move the file from path {file_path} to {destination_folder}")
         except Exception:
             error = InsuranceException("Error in module {0} class {1} method {2}".format(
                 Data_Validation.__module__, Data_Validation.__class__.__name__, self.move_to_bad_archive_directory.__name__), sys)
             raise error
 
-    def copy_to_good_data_directory(self, folder_path, file_name: str):
+    def copy_to_good_data_directory(self, file_path: str):
         """Copy the good file to Good Data Directory
 
         Args:
@@ -52,9 +55,9 @@ class Data_Validation:
                 Good_Data_Directory, utility.get_date(), utility.get_time())
             self.create_good_data_directory(destination_folder)
             shutil.copy(
-                os.path.join(folder_path, file_name), destination_folder)
-            self.__logger.log(
-                f"Successfully move the file {file_name} to {destination_folder}")
+                file_path, destination_folder)
+            self.__logger.log( 
+                f"Successfully move the file from path {file_path} to {destination_folder}")
         except Exception:
             error = InsuranceException("Error in module {0} class {1} method {2}".format(
                 Data_Validation.__module__, Data_Validation.__class__.__name__, self.copy_to_good_data_directory.__name__), sys)
@@ -66,39 +69,72 @@ class Data_Validation:
             path (str): path of the Good Data directory
         """
         utility.create_directory(path)
-    def create_bad_data_directory(self,path):
+    def create_bad_data_directory(self,path:str):
         """Responsible for creating Bad Data directory
 
         Args:
             path (str): path of the Bad Data directory
         """
         utility.create_directory(path)
-    def validate_file_name(self,batch_directory_path):
+
+    def validate_file_name(self, file_name: str,file_path:str):
+        """For validating the name of the file with the given schema
+
+        Args:
+            file_name (str): Give the name of the file
+            file_path (str): Path of the file.
+        """
         self.__logger.log("Filename validation started....")
-        files = os.listdir(batch_directory_path)
         LengthOfDateStampInFile, LengthOfTimeStampInFile, _, _ = utility.read_training_schema()
         match_string = self.return_regular_expression()
-        for file_name in files:
-            if not file_name.endswith('.csv'):
-                continue
-            datestamp_length = len(file_name.split('_')[1])
-            timestamp_length = len(file_name.split('_')[2].split('.csv')[0])
-            if re.match(match_string, file_name) and datestamp_length == LengthOfDateStampInFile and timestamp_length == LengthOfTimeStampInFile:
-                self.copy_to_good_data_directory(os.path.join(
-                    os.getcwd(), batch_directory_path), file_name)
-                self.__logger.log(f"Filename {file_name} is match with the regex so moving to Good Data Directory path")
-            else:
-                self.move_to_bad_archive_directory(
-                    os.path.join(os.getcwd(), batch_directory_path), file_name)
-                self.__logger.log(f"Filename {file_name} is not match with the regex so moving to Bad Data Directory path")
-def validation_main(datasource,collection_name,config,is_log_enabled):
+        datestamp_length = len(file_name.split('_')[1])
+        timestamp_length = len(file_name.split('_')[2].split('.csv')[0])
+        if re.match(match_string, file_name) and datestamp_length == LengthOfDateStampInFile and timestamp_length == LengthOfTimeStampInFile:
+            self.copy_to_good_data_directory(os.path.join(
+                os.getcwd(), file_path))
+            self.__logger.log(f"Filename {file_name} is match with the regex so moving to Good Data Directory path")
+            return 1
+        else:
+            self.move_to_bad_archive_directory(
+                os.path.join(os.getcwd(), file_path))
+            self.__logger.log(f"Filename {file_name} is not match with the regex so moving to Bad Data Directory path")
+            return 0
+    def validate_column_names(self, df, file_path):
+        df_columns = df.columns
+        LengthOfDateStampInFile, LengthOfTimeStampInFile, NumberofColumns, ColName = read_training_schema()
+        print("ColNames",list(ColName.keys()))
+        print("df_columns", df_columns, 'file', file_path)
+        if list(ColName.keys()) != df_columns:
+            self.move_to_bad_archive_directory(
+                file_path)
+
+
+def validation_main(datasource,collection_name:str,config,is_log_enabled:bool):
+    """Responsible to execute the pipeline.
+
+    Args:
+        datasource (str): Path of the data
+        collection_name (str): Name of the collection in MongoDb database.
+        config (str): Configuration file.
+        is_log_enabled (bool): If it is set to True then only it will write the logs. Defaults to True.
+    """
     logger = utility.get_log_object_for_training(
         collection_name=collection_name, is_log_enabled=is_log_enabled)
     logger.log("Validate main started....")
     data_validation = Data_Validation(logger=logger, config=config)
     logger.log("Data Validation Run Successfully....")
-    data_validation.validate_file_name(datasource)
-    logger.log("validate_file_name funtion Run Successfully....")
+    loader = Data_Loader(is_log_enabled)
+    for file_name in os.listdir(datasource):
+        if file_name.endswith(".csv"):
+            file_path=os.path.join(os.getcwd(),datasource,file_name)
+            if data_validation.validate_file_name( file_name, file_path):
+                logger.log("validate_file_name funtion Run Successfully....")
+                logger.log("load_data_from_path funtion Started....")
+                df = loader.load_data_from_path(file_path)
+                logger.log("validate_column_names funtion Started....")
+                data_validation.validate_column_names(df,file_path)
+                logger.log(
+                    "validate_column_names funtion Run Successfully....")
 
 if __name__=='__main__':
     args=argparse.ArgumentParser()
@@ -107,6 +143,9 @@ if __name__=='__main__':
     args.add_argument("--is_log_enabled", default=True)
     args.add_argument("--data_source", default=os.path.join(os.getcwd(), utility.read_params()['batch_file']['training_batch_files_path']))
     parsed_args=args.parse_args()
+    if (parsed_args.is_log_enabled)==str:
+        parsed_args.is_log_enabled = ast.literal_eval(
+            parsed_args.is_log_enabled.title())
     validation_main(datasource=parsed_args.data_source,
                     collection_name=parsed_args.log_collection_name, config=parsed_args.config, is_log_enabled=parsed_args.is_log_enabled)
     
