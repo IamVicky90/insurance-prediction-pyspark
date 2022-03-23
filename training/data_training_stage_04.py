061788572441535fef61107aa7a82a4ac8174b15
Vicky <waqasbilal02@gmail.com>
2022-03-23 19:46:14 +0500

    model_training class completed

--- training/data_training_stage_04.py
+++ training/data_training_stage_04.py
@@ -6,6 +6,9 @@ from pyspark.ml.feature import StringIndexer
 from pyspark.ml.feature import VectorAssembler
 from pyspark.ml import Pipeline
 from pyspark.sql.types import IntegerType, StringType, FloatType
+from pyspark.ml.regression import RandomForestRegressor
+import numpy as np
+from sklearn.metrics import r2_score, mean_squared_error
 log_collection_name = "Training_model"
 class Data_Preprocessing:
     def __init__(self,logger,is_log_enabled: bool,dataframe,pipeline_path:str):
@@ -46,7 +49,7 @@ class Data_Preprocessing:
             test_size (float, optional):size of the test data. Defaults to 0.2.
             random_state (float, optional):Random State. Defaults to 42.
         """
-        df_train, df_test = self.__dataframe.randomSplit(
+        return self.__dataframe.randomSplit(
             weights=[1-test_size, test_size], seed=random_state)
 
     def update_dataframe_scheme(self, schema_definition):
@@ -54,7 +57,6 @@ class Data_Preprocessing:
         for col_name, dtype in schema_definition.items():
             self.__dataframe = self.__dataframe.withColumn(
                 col_name, self.__dataframe[col_name].cast(dtype))
-        print('.......................................')
         print(self.__dataframe.printSchema())
     def get_prepaired_data_set(self):
         schema_definition = {"age": IntegerType(),
@@ -77,11 +79,74 @@ class Data_Preprocessing:
         return self.splitting_the_dataframe(test_size=float(utility.read_params()[
             'base']['test_size']), random_state=int(utility.read_params()[
                 'base']['random_state']))
-        
+class model_training:
+    def __init__(self,logger,is_log_enabled,config):
+        """Responsible for training of the model.
+
+        Args:
+            logger (logger_object): Object of the logger.
+            is_log_enabled (bool): If it is set to True then only it will write the logs. Defaults to True.
+            config (str): path of the config file.
+        """
+        self.__logger = logger
+        self.__config = utility.read_params(config)
+        self.__is_log_enabled=is_log_enabled
+        self.__ss = Spark_Manager().get_SparkSession()
+        self.model_path=self.__config['artifacts']['model']['model_path']
+    def get_dataframe(self):
+        master_file_path=os.path.join(self.__config['artifacts']['Data_Directories']['training']['master_csv_path'],
+        self.__config['artifacts']['Data_Directories']['training']['master_csv_filename'])
+        df = self.__ss.read.csv(
+            path=master_file_path, header=True, inferSchema=True)
+        return df
+    def get_prepared_data(self):
+        """Prepared the dataset from Data_Preprocessing class
+        """
+        pipelining_path=self.__config['artifacts']['Data_Directories']['training']['pipelining_path']
+        dp = Data_Preprocessing(self.__logger, True,
+                                self.get_dataframe(), pipelining_path)
+        return dp.get_prepaired_data_set()
+    def start_training(self):
+        """Responsible for triggering the training process of the model
+        """
+        df_train,df_test=self.get_prepared_data()
+        random_forest_regressor = RandomForestRegressor(
+            featuresCol="input_features", labelCol="expenses")
+        random_forest_model = random_forest_regressor.fit(df_train)
+        train_prediction = random_forest_model.transform(df_train)
+        testing_prediction = random_forest_model.transform(df_test)
+        training_data = train_prediction.select(
+            "expenses", "prediction").toPandas()
+        testing_data = testing_prediction.select(
+            "expenses", "prediction").toPandas()
+        self.save_regression_metric_data(training_data['expenses'], training_data['prediction'],
+                                         title="Training score")
+        self.save_regression_metric_data(testing_data['expenses'], testing_data['prediction'],
+                                         title="Testing score")
+
+        self.save_model(model=random_forest_model,
+                        model_name=self.__config['artifacts']['model']['model_name'])
+        self.__ss.stop()
+
+    def save_model(self, model, model_name, intermediate_path=None):
+
+        if intermediate_path is None:
+            model_path = os.path.join(self.model_path)
+        else:
+            model_path = os.path.join(self.model_path, intermediate_path)
+        utility.create_directory(model_path )
+        model_full_path = os.path.join(model_path, f"{model_name}")
+        model.write().overwrite().save(model_full_path)
+
+    def save_regression_metric_data(self, y_true, y_pred, title):
+        rmse = np.sqrt(mean_squared_error(y_true, y_pred))
+        r_squared_score = r2_score(y_true, y_pred)
+        msg = f"{title} R squared score: {r_squared_score:.3%}"
+        print(msg)
+        msg = f"{title} Root mean squared error: {rmse:.3}"
+        print(msg)
+    
 
 if __name__ =="__main__":
-    ss = Spark_Manager().get_SparkSession()
-    df = ss.read.csv(
-       path= "artifacts/training_data/master_csv/Master.csv", header=True, inferSchema=True)
-    dp=Data_Preprocessing('',True,df,'pipelining')
-    dp.get_prepaired_data_set()
+    mt = model_training('', True, 'config/params.yaml')
+    mt.start_training()
